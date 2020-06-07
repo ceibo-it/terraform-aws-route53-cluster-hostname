618af16d52e14216bcba09fa67ff39f1a18eb479
federicopires <59855798+federicopires@users.noreply.github.com>
2020-06-07 15:05:50 -0300

    Add alias support (#1)

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
 resource "aws_route53_record" "default" {
-  count   = var.enabled ? 1 : 0
+  count   = var.enabled && var.alias ? 0 : 1
   zone_id = var.zone_id
   name    = var.name
   type    = var.type
   ttl     = var.ttl
   records = var.records
+}
+
+
+resource "aws_route53_record" "default-alias" {
+  count = var.enabled && var.alias ? 1 : 0
+
+  alias {
+    name                   = var.alias_name
+    zone_id                = var.alias_target_zone_id
+    evaluate_target_health = var.evaluate_target_health
+  }
+
+  zone_id = var.zone_id
+  name    = var.name
+  type    = var.type
 }
--- variables.tf
+++ variables.tf
@@ -14,9 +14,34 @@ variable "zone_id" {
   description = "Route53 DNS Zone ID"
 }
 
+variable "alias" {
+  type        = bool
+  description = "Set to true if this is an alias record"
+  default     = false
+}
+
+variable "alias_name" {
+  type        = string
+  description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
+  default     = ""
+}
+
+variable "alias_target_zone_id" {
+  type        = string
+  description = "Route53 DNS Zone ID"
+  default     = ""
+}
+
+variable "evaluate_target_health" {
+  type        = string
+  description = "Valid only when alias=true. Set to true if you want Route 53 to determine whether to respond to DNS queries using this resource record set by checking the health of the resource record set. "
+  default     = false
+}
+
 variable "records" {
   type        = list(string)
-  description = "DNS records to create"
+  description = "DNS records to create. Required for non-alias records."
+  default     = [""]
 }
 
 variable "type" {
